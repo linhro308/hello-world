0916221e0a418f85fd620690c25c6a44c1ac1426
LinhBuiManh <linhbm2@viettel.com.vn>
2023-05-22 05:41:19 -0400

    update

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello World")
+def build_ospf_config_1(vrf,rd, rt_export='', rt_import=''):
+    rt_export = rd if not rt_export else rt_export
+    rt_import = rd if not rt_import else rt_import
+    return f"""
+    vrf: {vrf}
+    rd : {rd}
+    rt_import : {rt_import}
+    rt_export : {rt_export}
+    """
+print (build_ospf_config_1(1,2,3))
